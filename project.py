f802dfc632a86d6fd775fe0cece5c46f2045b308
harshsinghrana <harshsinghrana006@gmail.com>
2024-08-27 18:50:07 +0530

    macD

--- project.py
+++ project.py
@@ -16,3 +16,95 @@ df = pd.read_csv("mcdonalds.csv")
 df.head(3)
 
 df.shape
+
+df.isnull().sum()
+
+def EDA(df,figuresize):
+    cat = []
+    con = []
+    from warnings import filterwarnings
+    filterwarnings("ignore")
+    u = int((df.shape[1]+1)/2)
+    plt.figure(figsize=figuresize)
+    x = 1
+
+    for i in df.columns:
+        if(df[i].dtypes == "object"):
+            cat.append(i)
+            plt.subplot(u,2,x)
+            x = x+1
+            sns.countplot(df[i])
+        else:
+            con.append(i)
+            plt.subplot(u,2,x)
+            x = x+1
+            sns.distplot(df[i])
+
+    print(df.corr())
+
+
+EDA(df, (20, 30))
+
+labels = ['Female', 'Male']
+size = df['Gender'].value_counts()
+colors = ['pink', 'crimson']
+explode = [0, 0.1]
+plt.rcParams['figure.figsize'] = (7, 7)
+plt.pie(size, colors = colors, explode = explode, labels = labels, shadow = True, autopct = '%.2f%%')
+plt.title('Gender', fontsize = 20)
+plt.axis('off')
+plt.legend()
+plt.show()
+plt.rcParams['figure.figsize'] = (25, 8)
+data = sns.countplot(x=df['Age'], palette = 'viridis')
+data.bar_label(data.containers[0])
+plt.title('Age distribution of customers', fontsize = 20)
+plt.show()
+
+df['Like'] = df['Like'].replace({'I hate it!-5': '-5','I love it!+5':'+5'})
+
+sns.catplot(data=df, x="Like", y="Age", orient="v", height=5, aspect=2, palette="Set2",kind="swarm")
+plt.title('Likelyness of McDonald w.r.t Age', fontsize=20)
+plt.show()
+
+df_new = df.drop(labels=['Like','Age','VisitFrequency','Gender'], axis=1)
+
+df_new.head()
+
+df_encoded = df_new.apply(LabelEncoder().fit_transform)
+df_encoded.head()
+
+pca_data = preprocessing.scale(df_encoded)
+pca = PCA(n_components=11)
+pc = pca.fit_transform(pca_data)
+names = ['pc1','pc2','pc3','pc4','pc5','pc6','pc7','pc8','pc9','pc10','pc11']
+pf = pd.DataFrame(data = pc, columns = names)
+pf.head()
+
+pca.explained_variance_ratio_
+
+
+pca_scores = PCA().fit_transform(pca_data)
+
+cluster.biplot(cscore=pca_scores, loadings=loadings, labels=df.columns.values,
+               var1=round(pca.explained_variance_ratio_[0]*100, 2),
+    var2=round(pca.explained_variance_ratio_[1]*100, 2),show=True,dim=(10,5))
+
+model = KMeans()
+visualizer = KElbowVisualizer(model, k=(1,12)).fit(df_encoded)
+visualizer.show();
+
+kmeans = KMeans(n_clusters=4, init='k-means++', random_state=0).fit(df_encoded)
+df['cluster_num'] = kmeans.labels_ 
+print ('Labels:', kmeans.labels_)
+print ('WCSS:', kmeans.inertia_)
+print('No. of iterations: ', kmeans.n_iter_)
+print('Cluster centroids: ', kmeans.cluster_centers_)
+print('Cluster size: ', Counter(kmeans.labels_))
+
+sns.scatterplot(data=pf, x="pc1", y="pc2", hue=kmeans.labels_)
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], marker="X", c="r", s=80, label="centroids")
+plt.legend()
+plt.show()
+
+sns.boxplot(x="cluster_num", y="Age", data=df);
